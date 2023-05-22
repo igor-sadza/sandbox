f042d9ba51764b54170c36eb77d85bf902d37423
root <root@nvim.mshome.net>
2023-05-22 01:48:22 +0000

    clearup terraform resource, added ansible config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,8 @@
 resource "null_resource" "main" { 
-
-  # ssh connection block 
-  connection {
-    type      = "ssh" 
-    user      = var.ssh_user 
-    password  = var.ssh_pass 
-    host      = var.ssh_host 
-    port      = var.ssh_port 
-  }
-
-  # move to /tmp ~ ansible 
-  provisioner "file" {
-    source        = "../ansible"
-    destination   = "/tmp"
-  }
-
-  # install ansible 
-  provisioner "remote-exec"  {
-    inline = [
-      "apt-get update -qq",
-      "apt-get install -y -qq ansible"     
-    ]
-  }
   
   # execute ansible 
   provisioner "local-exec"  {
-    command = "ansible-playbook -i ./ansible/inventory/inventory.yml ./ansible/playbooks/install_driver_nvidia.yml" 
+    command = "ansible-playbook -e 'ANSIBLE_CONFIG=./ansible/ansible.cfg' -i ./ansible/inventory/inventory.yml ./ansible/playbooks/install_driver_nvidia.yml" 
     working_dir = "../${path.module}"
   }
 }
