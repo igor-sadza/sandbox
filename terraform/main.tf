57dde883a54c91e3a76d4d6eb5b0b12a868b7954
root <root@nvim.mshome.net>
2023-05-22 01:56:53 +0000

    temporary fix for 49254

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "main" {
   
   # execute ansible 
   provisioner "local-exec"  {
-    command = "ansible-playbook -e 'ANSIBLE_CONFIG=./ansible/ansible.cfg' -i ./ansible/inventory/inventory.yml ./ansible/playbooks/install_driver_nvidia.yml" 
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./ansible/inventory/inventory.yml ./ansible/playbooks/install_driver_nvidia.yml" 
     working_dir = "../${path.module}"
   }
 }
